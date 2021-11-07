fe72ad4fc7b50166bf470a932edd3ee6efde2291
aravind <aravindkannan2001@gmail.com>
2021-11-08 00:19:12 +0530

    Ex01: Deleted a few files

--- Ex01/Q3 - ifconfig.sh
+++
@@ -1,9 +0,0 @@
-# Q3 - ifconfig
-# Used to configure the kernel-resident network interfaces
-ifconfig
-
-# -a: display all interfaces which are currently available, even if down
-ifconfig -a
-
-# -s: display a short list (like netstat -i)
-ifconfig -s
--- Ex01/Q5 - traceroute.sh
+++
@@ -1,7 +0,0 @@
-# Q5 - traceroute
-# Tracks the route packets taken from an IP network 
-# on their way to a given host
-traceroute gmail.com
-
-# Outputs a complete result
-traceroute --icmp gmail.com
